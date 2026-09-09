7ce95ce7dd420a4081f9ff6a6ee4bbb4d57dcfac
AmbitiousBoy2001 <push16102001@gmail.com>
2026-09-09 23:14:58 +0530

    Tracker First Comment

---
+++ expense/home/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+TYPE = (
+    ('Positive', 'Positive'),
+    ('Negative', 'Negative')
+)
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    income = models.FloatField()
+    expenses = models.FloatField(default=0)
+    balance = models.FloatField(blank=True, null=True)
+
+
+
+class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=100)
+    amount = models.FloatField(null=True)
+    expense_type = models.CharField(max_length=100, choices=TYPE)
+
+    def __str__(self):
+        return self.name
